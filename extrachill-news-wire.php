a37968ae39de2fe2d8cfce9f431d0833ec2bd0e6
Chris Huber <chubes@extrachill.com>
2026-07-12 16:49:10 -0400

    feat: notify festival subscribers of wire updates (#15)

--- extrachill-news-wire.php
+++ extrachill-news-wire.php
@@ -22,6 +22,7 @@ define( 'FESTIVAL_WIRE_TEMPLATE_DIR', FESTIVAL_WIRE_PLUGIN_DIR . 'templates/' );
 require_once FESTIVAL_WIRE_INCLUDE_DIR . 'festival-wire-post-type.php';
 require_once FESTIVAL_WIRE_INCLUDE_DIR . 'festival-wire-query-filters.php';
 require_once FESTIVAL_WIRE_INCLUDE_DIR . 'festival-metadata.php';
+require_once FESTIVAL_WIRE_INCLUDE_DIR . 'festival-wire-notifications.php';
 require_once FESTIVAL_WIRE_INCLUDE_DIR . 'theme-integration.php';
 require_once FESTIVAL_WIRE_INCLUDE_DIR . 'core/breadcrumbs.php';
 require_once FESTIVAL_WIRE_INCLUDE_DIR . 'core/post-meta.php';
