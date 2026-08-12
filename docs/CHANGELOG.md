1ed4c2e3411e3c4d1403d872c53ee9f5eaf126d7
Chris Huber <chubes@extrachill.com>
2026-08-12 14:12:59 +0000

    release: v0.6.0

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [0.6.0] - 2026-08-12
+
+### Added
+- expose bounded recent Wire activity
+
 ## [0.5.2] - 2026-07-29
 
 ### Changed
--- extrachill-news-wire.php
+++ extrachill-news-wire.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Extra Chill News Wire
  * Description: Festival Wire custom post type and functionality for music festival coverage with fast-loading archives and template overrides.
- * Version: 0.5.2
+ * Version: 0.6.0
  * Author: Chris Huber
  * Text Domain: extrachill
  * Domain Path: /languages
